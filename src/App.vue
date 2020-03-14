88859a87fb6ec3bf3348fd25d7ee65202bc41ec8
Niimi Youki <niimiyouki@gmail.com>
2020-03-14 17:25:20 +0900

    テスト作成

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld @input="hoge" @input2="fuga" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    hoge(e){
+      // test1 送られてくる引数がpromiseなので、こっちでthenを付ける。
+      e.then(result => alert(result))
+    },
+    fuga(e){
+      // test2 引数はただの数値なので、そのまま表示。受け側としてはこっちが自然
+      alert(e)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="$emit('input', timer())">TEST1</button>{{ /* こっちは上手くいく */}}
+    <button @click="timer().then(result => $emit('input2', result))">TEST2</button>{{ /* これだと動くけどエラーが出る… */}}
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    async timer(){
+      return new Promise((resolve)=> {
+        setTimeout(()=>{resolve('test')}, 3000)
+      })
+    }
+  }
+}
+</script>
